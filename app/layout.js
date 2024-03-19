eceb227dbc28f03f377832891301b2835d7cb1d7
shashikunal <shashikunal@gmail.com>
2024-03-19 15:38:14 +0530

    first commit

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,12 @@
-import { Inter } from "next/font/google";
+import { Poppins } from "next/font/google";
 import "./globals.css";
+import { cn } from "@/lib/utils";
 
-const inter = Inter({ subsets: ["latin"] });
+const poppins = Poppins({
+  subsets: ["latin"],
+  variable: "--font-poppins",
+  weight: ["100", "300", "400", "500", "600"],
+});
 
 export const metadata = {
   title: "Create Next App",
@@ -11,7 +16,14 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body
+        className={cn(
+          "relative h-full antialiased font-sans",
+          poppins.className
+        )}
+      >
+        <main className="relative flex flex-col min-h-screen">{children}</main>
+      </body>
     </html>
   );
 }
