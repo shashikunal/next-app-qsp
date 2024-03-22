445f1dda462cd2e32a158f0899071380f05544f1
shashikunal <shashikunal@gmail.com>
2024-03-22 10:36:42 +0530

    second commit

--- app/page.js
+++ app/page.js
@@ -1,13 +1,9 @@
-import { Button } from "@/components/ui/button";
-import { EnvelopeOpenIcon } from "@radix-ui/react-icons";
-import Image from "next/image";
+import CourseContainer from "@/components/courses/CourseContainer";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Button variant="outline">
-        <EnvelopeOpenIcon className="mr-2 h-4 w-4" /> Login with Email
-      </Button>
-    </main>
+    <>
+      <CourseContainer />
+    </>
   );
 }
---
+++ components/courses/CourseContainer.jsx
@@ -0,0 +1,43 @@
+import "./courses.css";
+
+const CourseContainer = () => {
+  return (
+    <section className="course_block">
+      <article>
+        <header>
+          <h2>Explore our Courses</h2>
+        </header>
+        <main className="main_class">
+          <aside className="sidebar">
+            <h4>popular courses</h4>
+          </aside>
+          <aside className="main_content">
+            <header>Menu</header>
+            <main>
+              <div id="course_block">
+                <h4>lorem5</h4>
+              </div>
+              <div id="course_block">
+                <h4>lorem5</h4>
+              </div>
+              <div id="course_block">
+                <h4>lorem5</h4>
+              </div>
+              <div id="course_block">
+                <h4>lorem5</h4>
+              </div>
+              <div id="course_block">
+                <h4>lorem5</h4>
+              </div>
+              <div id="course_block">
+                <h4>lorem5</h4>
+              </div>
+            </main>
+          </aside>
+        </main>
+      </article>
+    </section>
+  );
+};
+
+export default CourseContainer;
